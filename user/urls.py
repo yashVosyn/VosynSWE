7548bb6cc7b45ebd236a96c59f364eebb4f68a3f
yashVosyn <yashshah0348@gmail.com>
2023-12-07 09:29:42 -0500

    user

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
-    path('signup',views.signup,name="signup")
+    path('signup',views.signup,name="signup"),
+    path('signin',views.signin,name="signin"),
+    path('signout',views.signout,name="signout"),
     
 ]
--- user/views.py
+++ user/views.py
@@ -1,10 +1,52 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import redirect,render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
 # Create your views here.
 def home(request):
     return render(request,"user/index.html")
 
 def signup(request):
-    return HttpResponse("hello this is signup page")
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        myuser = User.objects.create_user(username,email,pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        
+        myuser.save()
+        
+        messages.success(request,"your account has been succesfully created.")
+        
+        return redirect("signin")
+        
+    return render(request,"user/signup.html")
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['pass1']
+        
+        
+        user = authenticate(username = username, password = password)
+        
+        if user is not None:
+            login(request,user)
+            fname = user.first_name
+            return render(request,"authentication/index.html",{'fname':fname})
+            
+        else:
+            messages.error(request,'Bad Credentials')
+            return redirect('home')            
+            
+    return render(request,"user/signin.html")
+def signout(request):
+    pass
         
